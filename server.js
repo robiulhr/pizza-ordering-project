776f71d9dba6f4a4910bcdfb14ddb8deba9490b9
robiulhasanrohan <robiul@blogdesire.com>
2021-09-19 09:46:40 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const ejs = require("ejs")
+const path = require("path")
+const expressLayout = require("express-ejs-layouts")
+const PORT = process.env.PORT || 3000
+app.get("/", (req, res) => {
+    res.render("home")
+})
+// set Templete engine
+app.use(expressLayout)
+app.set("views", path.join(__dirname, "/resources/views"));
+console.log(__dirname);
+app.set('view engine', 'ejs');
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
